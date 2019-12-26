8208bc65091b89cf0340e703005d9e46373f67ba
SankarBhagawanPalla <54730150+SankarBhagawanPalla@users.noreply.github.com>
2019-12-26 12:52:44 -0500

    Initial Project Structure commit

---
+++ src/main/java/com/springworks/savory/SavoryApplication.java
@@ -0,0 +1,13 @@
+package com.springworks.savory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SavoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springworks/savory/SavoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springworks.savory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
