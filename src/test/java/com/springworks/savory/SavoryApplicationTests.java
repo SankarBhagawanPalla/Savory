c33eed06ce16c0e5f2d49b074e786255b3c20018
SankarBhagawanPalla <54730150+SankarBhagawanPalla@users.noreply.github.com>
2020-01-11 13:35:29 -0500

    Added junit test Case for getRecipes

--- src/test/java/com/springworks/savory/SavoryApplicationTests.java
+++ src/test/java/com/springworks/savory/SavoryApplicationTests.java
@@ -2,6 +2,8 @@ package com.springworks.savory;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
 
 @SpringBootTest
 class SavoryApplicationTests {
