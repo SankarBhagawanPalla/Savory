77186e7b625c79066227579b869e28377a57b313
SankarBhagawanPalla <54730150+SankarBhagawanPalla@users.noreply.github.com>
2020-01-11 14:30:18 -0500

    Mockito ArgumentCapture to test

--- src/test/java/com/springworks/savory/controllers/IndexControllerTest.java
+++ src/test/java/com/springworks/savory/controllers/IndexControllerTest.java
@@ -1,17 +1,21 @@
 package com.springworks.savory.controllers;
 
+import com.springworks.savory.domain.Recipe;
 import com.springworks.savory.services.RecipeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 class IndexControllerTest {
 
@@ -29,10 +33,29 @@ class IndexControllerTest {
     @Test
     void getIndexPage() {
 
+        //given
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(new Recipe());
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        recipes.add(recipe);
+        Recipe recipe2 = new Recipe();
+        recipe2.setId(2L);
+        recipes.add(recipe2);
+
+
+        when(recipeService.getRecipes()).thenReturn(recipes);
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        //when
         String value = indexController.getIndexPage(model);
+
+        //then
         assertEquals("index",value);
-        verify(model, times(1)).addAttribute(eq("recipes"),anySet());
+        verify(model, times(1)).addAttribute(eq("recipes"),argumentCaptor.capture());
         verify(recipeService, times(1)).getRecipes();
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(3, setInController.size());
 
     }
 }
