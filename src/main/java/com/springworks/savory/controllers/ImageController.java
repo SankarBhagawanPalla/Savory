da187974104ad42899c6a0456c3ada9ecc6a0d4e
SankarBhagawanPalla <54730150+SankarBhagawanPalla@users.noreply.github.com>
2020-03-31 14:00:58 -0400

    Rendering Image from DB.

--- src/main/java/com/springworks/savory/controllers/ImageController.java
+++ src/main/java/com/springworks/savory/controllers/ImageController.java
@@ -1,7 +1,9 @@
 package com.springworks.savory.controllers;
 
+import com.springworks.savory.commands.RecipeCommand;
 import com.springworks.savory.services.ImageService;
 import com.springworks.savory.services.RecipeService;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 @Controller
 public class ImageController {
@@ -34,4 +41,22 @@ public class ImageController {
 
         return "redirect:/recipe/" + id + "/show";
     }
+
+    @GetMapping("recipe/{id}/recipeimage")
+    public void renderImagefromDB(@PathVariable String id, HttpServletResponse response) throws IOException {
+
+        RecipeCommand recipeCommand = recipeService.findCommandById(Long.valueOf(id));
+
+        byte[]  bytearray = new byte[recipeCommand.getImage().length];
+
+        int i=0;
+        for(Byte wrapperbyte: recipeCommand.getImage()){
+            bytearray[i++] = wrapperbyte;
+        }
+        response.setContentType("image/jpeg");
+        InputStream is = new ByteArrayInputStream(bytearray);
+        IOUtils.copy(is, response.getOutputStream());
+
+    }
+
 }
