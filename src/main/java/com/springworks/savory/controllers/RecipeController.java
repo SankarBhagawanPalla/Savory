e0bac111afb05becf9ab0c302c8cdaf646d2f08f
SankarBhagawanPalla <54730150+SankarBhagawanPalla@users.noreply.github.com>
2020-04-07 16:17:25 -0400

    Added Error Page and ExceptionHandler method.

--- src/main/java/com/springworks/savory/controllers/RecipeController.java
+++ src/main/java/com/springworks/savory/controllers/RecipeController.java
@@ -1,11 +1,14 @@
 package com.springworks.savory.controllers;
 
 import com.springworks.savory.commands.RecipeCommand;
+import com.springworks.savory.exceptions.NotFoundException;
 import com.springworks.savory.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @Slf4j
@@ -55,6 +58,17 @@ public class RecipeController {
         log.debug("Deleting id:"+ id);
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(){
+
+        log.error("Handling not found exception");
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("404error");
+        return modelAndView;
     }
 
 
