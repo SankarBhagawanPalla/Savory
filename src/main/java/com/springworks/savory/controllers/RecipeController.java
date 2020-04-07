21cb1ad708bf309648f26e012b907ca78ee49151
SankarBhagawanPalla <54730150+SankarBhagawanPalla@users.noreply.github.com>
2020-04-07 17:24:29 -0400

    Handling Bad request globally.

--- src/main/java/com/springworks/savory/controllers/RecipeController.java
+++ src/main/java/com/springworks/savory/controllers/RecipeController.java
@@ -71,16 +71,6 @@ public class RecipeController {
         modelAndView.addObject("exception", exception);
         return modelAndView;
     }
-    
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(NumberFormatException.class)
-    public ModelAndView handleNumberformat(Exception exception){
-
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("400error");
-        modelAndView.addObject("exception", exception);
-        return modelAndView;
-    }
 
 
 
